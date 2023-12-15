1e185373bd4c926befb04dba7399b55d293fcbb4
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2023-12-15 21:47:13 +0700

    [up] update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["pixner.net", "res.cloudinary.com"],
+  },
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
